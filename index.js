d8ca10c53fa64e8be9aa5313ee39c3b6db91a210
Jasa-Hollow <74333561+Jasa-Hollow@users.noreply.github.com>
2020-11-12 14:45:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let coffeeMaker = {
-  state: "Apagada", //Estado inicial por defecto
+	state: "Apagada", //Estado inicial por defecto
 	coffee: false,	//Estado inicial por defecto
 	water: false,	//Estado inicial por defecto
 	grinder: function(grind){ // Molino y su respectiva función.
@@ -44,6 +44,7 @@ function on(onOff, grinder, tank){
 	onOff(state)
 	coffeeMaker.grinder(grind)
 	coffeeMaker.boiler(boil)
+	coffeeMaker.brewer(brew)
 }
 // ----------------------------------
 
@@ -54,6 +55,7 @@ function grind(coffee){
 			let gramos = prompt("Ingresa la cantidad de gramos de café a moler")
 			if (Number(gramos)){
 				console.log("Se han molido: " + gramos + "g de café")
+				let coffee = true
 				i++
 			}else{
 				console.log("No se ha ingresado el café")
@@ -71,6 +73,7 @@ function boil(water){
 			let miliL = prompt("Ingresa la cantidad de agua a hervir")  
 			if (Number(miliL)){
 				console.log("Se han hervido: " + miliL + "mL de agua")
+				let water = true
 				i++
 			}else{
 				console.log("No se ha ingresado el agua")
@@ -81,5 +84,20 @@ function boil(water){
 	}
 }
 
-//Puesta en marcha del funcionamiento de la máquina.
+function brew(coffee, water){
+	let taza = cup.state
+	let i = 0
+	while (i < 2){
+		if (taza === "Vacía"){
+			console.log("¡Su café está listo para servir!")
+			taza = "Llena"
+			i++
+		}else if(taza === "Llena"){
+			console.log("¡Disfrute su bebida!")
+			i++
+		}
+	}
+}
+
+// Puesta en marcha del funcionamiento de la máquina de café.
 onOff(coffeeMaker.state)
