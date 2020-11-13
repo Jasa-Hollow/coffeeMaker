73d52c617fe635c4f2d740776733fcb71c9a9180
Jasa-Hollow <74333561+Jasa-Hollow@users.noreply.github.com>
2020-11-13 14:57:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-let coffeeMaker = {
-  state: "Apagada", //Estado inicial por defecto
+let coffeeMakerNR = {
+	state: "Apagada", //Estado inicial por defecto
 	coffee: false,	//Estado inicial por defecto
 	water: false,	//Estado inicial por defecto
 	grinder: function(grind){ // Molino y su respectiva función.
 		setTimeout(()=>{
 			console.log("...")
-			grind(this.coffee)
+			grind(this.coffee) // Callback de la función "moler"
 		}, 3000)
 	},
 	boiler: function(boil){ // Tanque de agua y caldera.
 		setTimeout(()=>{
 			console.log("...")
-			boil(this.water)
+			boil(this.water) // Callback de la función "hervir"
 		}, 3000)
 	},
 	brewer: function(brew){ //Cabeza de grupo, donde se vierten los materiales y se sirve el café.
 		setTimeout(()=>{
 			console.log("...")
-			brew(this.coffee, this.water)
-		}, 3000)
+			brew(this.coffee, this.water) // Callback de la función "brew"
+		}, 4000)
 	}
 }
 
-let cup = {
-	state: "Vacía"
-}
+let coffeeMakerR = Object.create(coffeeMakerNR) // Objeto que define a la máquina, cuando ya está casi lista para servir el café.
 
 function onOff(state){
 	if (state === "Apagada"){
-		console.log("La máquina está: " + state + ", se encenderá pronto")
+		console.log("La máquina está: " + state + ", se encenderá pronto") // Se muestra el estado por defecto.
 		setTimeout(()=>{
 			console.log("...")
-			on(onOff)
+			on(onOff) // Se llama la función para ejecutarla en caso de que esté Apagada.
 		}, 3000)
 	}else if(state === "Encendida"){
 		console.log("La máquina está: " + state)
 	}
 }
 
-// ----------------------------------
-function on(onOff, grinder, tank){
-	let state = "Encendida"
+function on(onOff){
+	let state = "Encendida" // Se cambia el estado de la máquina a encendido.
 	onOff(state)
-	coffeeMaker.grinder(grind)
-	coffeeMaker.boiler(boil)
+	coffeeMakerNR.grinder(grind) // Se pone en acción el molino.
+	coffeeMakerNR.boiler(boil) // Se pone en acción el tanque de agua y la caldera.
+	coffeeMakerR.brewer(brew) // Se pone en acción el "brewer", luego de los anteriores procesos.
 }
-// ----------------------------------
 
 function grind(coffee){
 	if (coffee === false){
 		let i = 0
 		while(i < 1){
-			let gramos = prompt("Ingresa la cantidad de gramos de café a moler")
+			let gramos = prompt("Ingresa la cantidad de gramos de café a moler") // Recarga de granos de café.
 			if (Number(gramos)){
 				console.log("Se han molido: " + gramos + "g de café")
+				coffeeMakerR.coffee = true // Se almacena en el nuevo objeto, que el café molido ya está listo.
 				i++
 			}else{
 				console.log("No se ha ingresado el café")
@@ -68,9 +66,10 @@ function boil(water){
 	if (water === false){
 		let i = 0
 		while(i < 1){
-			let miliL = prompt("Ingresa la cantidad de agua a hervir")  
+			let miliL = prompt("Ingresa la cantidad de agua a hervir") // Recarga de agua en el tanque.
 			if (Number(miliL)){
 				console.log("Se han hervido: " + miliL + "mL de agua")
+				coffeeMakerR.water = true // Se almacena en el nuevo objeto, que el agua hervida ya está lista.
 				i++
 			}else{
 				console.log("No se ha ingresado el agua")
@@ -81,5 +80,20 @@ function boil(water){
 	}
 }
 
-//Puesta en marcha del funcionamiento de la máquina.
-onOff(coffeeMaker.state)
+function brew(coffee, water){
+	let taza = "Vacía" ; let i = 0
+	while (i < 2){
+		if (taza === "Vacía" && coffee === true && water === true){ // Solo se ejecuta, siempre y cuando los demás procesos se hayan realizado.
+			console.log("¡Su café está listo para servir!")
+			taza = "Llena" // Se llena la taza.
+			i++
+		}else if(taza === "Llena"){
+			console.log("¡Disfrute su bebida!")
+			i++
+		}else{
+			console.log("No se puede servir el café debido a que no están listos los ingredientes...")
+		}
+	}
+}
+
+onOff(coffeeMakerNR.state) // Puesta en marcha, del funcionamiento de la máquina.
